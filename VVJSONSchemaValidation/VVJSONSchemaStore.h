6d412619d7e49b760fce4212145f488243c92638
Vlas Voloshin <argentumko@gmail.com>
2015-01-01 21:21:29 +1100

    Updated copyright year for all source files, happy new year!

--- VVJSONSchemaValidation/VVJSONSchemaStore.h
+++ VVJSONSchemaValidation/VVJSONSchemaStore.h
@@ -3,7 +3,7 @@
 //  VVJSONSchemaValidation
 //
 //  Created by Vlas Voloshin on 28/12/2014.
-//  Copyright (c) 2014 Vlas Voloshin. All rights reserved.
+//  Copyright (c) 2015 Vlas Voloshin. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- VVJSONSchemaValidationTests/VVJSONSchemaTestCase.h
+++ VVJSONSchemaValidationTests/VVJSONSchemaTestCase.h
@@ -3,7 +3,7 @@
 //  VVJSONSchemaValidation
 //
 //  Created by Vlas Voloshin on 30/12/2014.
-//  Copyright (c) 2014 Vlas Voloshin. All rights reserved.
+//  Copyright (c) 2015 Vlas Voloshin. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
