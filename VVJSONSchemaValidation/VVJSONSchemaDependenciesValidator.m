1573120982397af72941d7e1c119bf8fc437b0e3
Vlas Voloshin <argentumko@gmail.com>
2015-01-02 19:39:18 +1100

    Fixed another memory management issue with errors in dependencies validator.

--- VVJSONSchemaValidation/VVJSONSchemaDependenciesValidator.m
+++ VVJSONSchemaValidation/VVJSONSchemaDependenciesValidator.m
@@ -42,60 +42,66 @@ static NSString * const kSchemaKeywordDependencies = @"dependencies";
     id dependencies = schemaDictionary[kSchemaKeywordDependencies];
     
     // dependencies must be a dictionary
-    if ([dependencies isKindOfClass:[NSDictionary class]]) {
-        NSMutableDictionary *schemaDependencies = [NSMutableDictionary dictionary];
-        NSMutableDictionary *propertyDependencies = [NSMutableDictionary dictionary];
-        
-        // parse the dependencies
-        __block BOOL success = YES;
-        [dependencies enumerateKeysAndObjectsUsingBlock:^(NSString *propertyName, id dependencyObject, BOOL *stop) {
-            if ([dependencyObject isKindOfClass:[NSDictionary class]]) {
-                // dependency object is a dictionary - parse it as a schema dependency;
-                // schema will have scope extended by "/dependencies/#" where # is dependent property name
-                VVJSONSchemaFactory *dependencySchemaFactory = [schemaFactory factoryByAppendingScopeComponentsFromArray:@[ kSchemaKeywordDependencies, propertyName ]];
-                
-                VVJSONSchema *dependencySchema = [dependencySchemaFactory schemaWithDictionary:dependencyObject error:error];
-                if (dependencySchema != nil) {
-                    schemaDependencies[propertyName] = dependencySchema;
-                } else {
-                    *stop = YES;
-                    success = NO;
-                }
-            } else if ([dependencyObject isKindOfClass:[NSArray class]]) {
-                // dependency object is an array - parse it as a property dependency;
-                // each property names array must be non-empty and contain unique strings
-                for (id dependentProperty in dependencyObject) {
-                    if ([dependentProperty isKindOfClass:[NSString class]] == NO) {
-                        *stop = YES;
-                        success = NO;
-                        return;
-                    }
-                }
-                
-                NSSet *dependentPropertiesSet = [NSSet setWithArray:dependencyObject];
-                if (dependentPropertiesSet.count == 0 || dependentPropertiesSet.count != [dependencyObject count]) {
+    if ([dependencies isKindOfClass:[NSDictionary class]] == NO) {
+        if (error != NULL) {
+            *error = [NSError vv_JSONSchemaErrorWithCode:VVJSONSchemaErrorCodeInvalidSchemaFormat failingObject:schemaDictionary failingValidator:nil];
+        }
+        return nil;
+    }
+    
+    NSMutableDictionary *schemaDependencies = [NSMutableDictionary dictionary];
+    NSMutableDictionary *propertyDependencies = [NSMutableDictionary dictionary];
+    
+    // parse the dependencies
+    __block BOOL success = YES;
+    __block NSError *internalError = nil;
+    [dependencies enumerateKeysAndObjectsUsingBlock:^(NSString *propertyName, id dependencyObject, BOOL *stop) {
+        if ([dependencyObject isKindOfClass:[NSDictionary class]]) {
+            // dependency object is a dictionary - parse it as a schema dependency;
+            // schema will have scope extended by "/dependencies/#" where # is dependent property name
+            VVJSONSchemaFactory *dependencySchemaFactory = [schemaFactory factoryByAppendingScopeComponentsFromArray:@[ kSchemaKeywordDependencies, propertyName ]];
+            
+            VVJSONSchema *dependencySchema = [dependencySchemaFactory schemaWithDictionary:dependencyObject error:&internalError];
+            if (dependencySchema != nil) {
+                schemaDependencies[propertyName] = dependencySchema;
+            } else {
+                *stop = YES;
+                success = NO;
+            }
+        } else if ([dependencyObject isKindOfClass:[NSArray class]]) {
+            // dependency object is an array - parse it as a property dependency;
+            // each property names array must be non-empty and contain unique strings
+            for (id dependentProperty in dependencyObject) {
+                if ([dependentProperty isKindOfClass:[NSString class]] == NO) {
                     *stop = YES;
                     success = NO;
                     return;
                 }
-                
-                propertyDependencies[propertyName] = dependentPropertiesSet;
-            } else {
-                // invalid dependency object
+            }
+            
+            NSSet *dependentPropertiesSet = [NSSet setWithArray:dependencyObject];
+            if (dependentPropertiesSet.count == 0 || dependentPropertiesSet.count != [dependencyObject count]) {
                 *stop = YES;
                 success = NO;
+                return;
             }
-        }];
-        
-        if (success) {
-            return [[self alloc] initWithSchemaDependencies:schemaDependencies propertyDependencies:propertyDependencies];
+            
+            propertyDependencies[propertyName] = dependentPropertiesSet;
+        } else {
+            // invalid dependency object
+            *stop = YES;
+            success = NO;
         }
-    }
+    }];
     
-    if (error != NULL) {
-        *error = [NSError vv_JSONSchemaErrorWithCode:VVJSONSchemaErrorCodeInvalidSchemaFormat failingObject:schemaDictionary failingValidator:nil];
+    if (success) {
+        return [[self alloc] initWithSchemaDependencies:schemaDependencies propertyDependencies:propertyDependencies];
+    } else {
+        if (error != NULL) {
+            *error = internalError ?: [NSError vv_JSONSchemaErrorWithCode:VVJSONSchemaErrorCodeInvalidSchemaFormat failingObject:schemaDictionary failingValidator:nil];
+        }
+        return nil;
     }
-    return nil;
 }
 
 - (NSSet *)subschemas
@@ -130,14 +136,20 @@ static NSString * const kSchemaKeywordDependencies = @"dependencies";
     }
     
     // validate schema dependencies
+    __block NSError *internalError;
     [self.schemaDependencies enumerateKeysAndObjectsUsingBlock:^(NSString *property, VVJSONSchema *schema, BOOL *stop) {
         if ([propertyNames containsObject:property]) {
-            if ([schema validateObject:instance withError:error] == NO) {
+            if ([schema validateObject:instance withError:&internalError] == NO) {
                 success = NO;
                 *stop = YES;
             }
         }
     }];
+    if (success == NO) {
+        if (error != NULL) {
+            *error = internalError;
+        }
+    }
     
     return success;
 }
