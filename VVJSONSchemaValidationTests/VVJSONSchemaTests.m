d2d398bf476e775990e57291ad5921ec17df9ea1
Vlas Voloshin <argentumko@gmail.com>
2015-01-08 02:14:19 +1100

    Updated test suite by adding "remote" schemas and refRemote test case.

--- VVJSONSchemaValidationTests/VVJSONSchemaTests.m
+++ VVJSONSchemaValidationTests/VVJSONSchemaTests.m
@@ -27,11 +27,13 @@ extern uint64_t dispatch_benchmark(size_t count, void (^block)(void));
 {
     [super setUp];
 
+    // prepare URLs of test cases
     NSArray *urls = [[NSBundle bundleForClass:[self class]] URLsForResourcesWithExtension:@"json" subdirectory:nil];
     if (urls.count == 0) {
         XCTFail(@"No JSON test cases found.");
     }
     
+    // load all test cases
     NSMutableArray *testSuite = [NSMutableArray array];
     for (NSURL *url in urls) {
         NSArray *testCases = [VVJSONSchemaTestCase testCasesWithContentsOfURL:url];
@@ -44,7 +46,48 @@ extern uint64_t dispatch_benchmark(size_t count, void (^block)(void));
     
     _testSuite = [testSuite copy];
     
+    // load reference schemas
+    _referenceStorage = [self.class remoteSchemasReferenceStorage];
+    
     NSLog(@"Loaded %lu test cases.", (unsigned long)testSuite.count);
+}
+
++ (VVJSONSchemaStorage *)remoteSchemasReferenceStorage
+{
+    static NSString * const kBaseRemoteSchemasURIString = @"http://localhost:1234/";
+
+    VVMutableJSONSchemaStorage *storage = [VVMutableJSONSchemaStorage storage];
+    NSURL *baseRemoteSchemasURI = [NSURL URLWithString:kBaseRemoteSchemasURIString];
+    
+    NSArray *urls = [[NSBundle bundleForClass:[self class]] URLsForResourcesWithExtension:@"json" subdirectory:@"remotes"];
+    for (NSURL *url in urls) {
+        NSString *documentName = url.lastPathComponent;
+        NSURL *schemaURI = [NSURL URLWithString:documentName relativeToURL:baseRemoteSchemasURI];
+        [self addSchemaFromURL:url withScopeURI:schemaURI intoStorage:storage];
+    }
+    
+    NSArray *subfolderURLs = [[NSBundle bundleForClass:[self class]] URLsForResourcesWithExtension:@"json" subdirectory:@"remotes/folder"];
+    for (NSURL *url in subfolderURLs) {
+        NSString *documentName = url.lastPathComponent;
+        NSURL *schemaURI = [NSURL URLWithString:[@"folder" stringByAppendingPathComponent:documentName] relativeToURL:baseRemoteSchemasURI];
+        [self addSchemaFromURL:url withScopeURI:schemaURI intoStorage:storage];
+    }
+    
+    return [storage copy];
+}
+
++ (void)addSchemaFromURL:(NSURL *)url withScopeURI:(NSURL *)scopeURI intoStorage:(VVMutableJSONSchemaStorage *)storage
+{
+    NSData *schemaData = [NSData dataWithContentsOfURL:url];
+    VVJSONSchema *schema = [VVJSONSchema schemaWithData:schemaData baseURI:scopeURI referenceStorage:nil error:NULL];
+    if (schema == nil) {
+        [NSException raise:NSInternalInconsistencyException format:@"Failed to instantiate reference schema from %@.", url];
+    }
+    
+    BOOL success = [storage addSchema:schema];
+    if (success == NO) {
+        [NSException raise:NSInternalInconsistencyException format:@"Failed to add reference schema from %@ into the storage.", url];
+    }
 }
 
 - (void)testSchemasInstantiationOnly
