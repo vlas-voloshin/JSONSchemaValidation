5b88f692fda62c5a586164d67d0083a126afd353
Vlas Voloshin <argentumko@gmail.com>
2020-03-28 16:27:25 +1100

    Added watchOS target and updated the podspec to include both tvOS and watchOS targets.

---
+++ VVJSONSchemaValidation-watchOS/VVJSONSchemaValidation.h
@@ -0,0 +1,27 @@
+//
+//  VVJSONSchemaValidation.h
+//  VVJSONSchemaValidation-watchOS
+//
+//  Created by Vlas Voloshin on 28/03/2020.
+//  Copyright (c) 2015 Vlas Voloshin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VVJSONSchemaValidation.
+FOUNDATION_EXPORT double VVJSONSchemaValidationVersionNumber;
+
+//! Project version string for VVJSONSchemaValidation.
+FOUNDATION_EXPORT const unsigned char VVJSONSchemaValidationVersionString[];
+
+// Public headers
+#import <VVJSONSchemaValidation/VVJSONSchema.h>
+#import <VVJSONSchemaValidation/VVJSONSchemaStorage.h>
+#import <VVJSONSchemaValidation/VVJSONSchemaErrors.h>
+
+// Headers for extension
+#import <VVJSONSchemaValidation/VVJSONSchemaValidator.h>
+#import <VVJSONSchemaValidation/VVJSONSchemaReference.h>
+#import <VVJSONSchemaValidation/VVJSONSchemaFactory.h>
+#import <VVJSONSchemaValidation/VVJSONSchemaValidationContext.h>
+#import <VVJSONSchemaValidation/VVJSONSchemaFormatValidator.h>
